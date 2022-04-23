41e2483936ddf1ab5989473731e82ddd21eaf72e
mhaikalfikri <mhaikalfikri252@gmail.com>
2022-04-23 14:17:35 +0700

    fix name test case

--- src/main/java/com/hadir/web1/utils/TestCases.java
+++ src/main/java/com/hadir/web1/utils/TestCases.java
@@ -2,14 +2,16 @@ package com.hadir.web1.utils;
 
 public enum TestCases {
 
-	T1("Testing Login"), T2("Testing Dashboard"), T3("Testing Form Registration"),
-	T4("Testing Form Registration Invalid"), T5("Testing Staff"), T6("Testing Staff Invalid"), T7("Testing Register"),
-	T8("Testing Reporting Karyawan"), T9("Testing Self Registration"), T10("Testing Manage Tipe"),
-	T11("Testing Manage Tipe Invalid"), T12("Testing Manage Hari Cuti"), T13("Testing Manage Hari Cuti Invalid"),
-	T14("Testing Manage Absen Point"), T15("Testing Manage Absen Point Invalid"), T16("Testing Manage Posisi"),
-	T17("Testing Manage Posisi Invalid"), T18("Testing Manage Level"), T19("Testing Manage Level Invalid"),
-	T20("Testing Manage Setting"), T21("Testing Manage Setting Invalid"), T22("Testing Manage Departemen"),
-	T23("Testing Manage Departemen Invalid"), T24("Testing Login Invalid");
+	T1("Testing Login Page"), T2("Testing Dashboard Page"), T3("Testing Form Registration Page"),
+	T4("Testing Form Registration Invalid Page"), T5("Testing Staff Page"), T6("Testing Staff Invalid Page"),
+	T7("Testing Register Page"), T8("Testing Reporting Karyawan Page"), T9("Testing Self Registration Page"),
+	T10("Testing Manage Tipe Page"), T11("Testing Manage Tipe Invalid Page"), T12("Testing Manage Hari Cuti Page"),
+	T13("Testing Manage Hari Cuti Invalid Page"), T14("Testing Manage Absen Point Page"),
+	T15("Testing Manage Absen Point Invalid Page"), T16("Testing Manage Posisi Page"),
+	T17("Testing Manage Posisi Invalid Page"), T18("Testing Manage Level Page"),
+	T19("Testing Manage Level Invalid Page"), T20("Testing Manage Setting Page"),
+	T21("Testing Manage Setting Invalid Page"), T22("Testing Manage Departemen Page"),
+	T23("Testing Manage Departemen Invalid Page"), T24("Testing Login Invalid Page");
 
 	private String testName;
 
